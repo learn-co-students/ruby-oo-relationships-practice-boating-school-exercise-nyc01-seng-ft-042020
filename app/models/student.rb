1992b82b25478f7f8b42a31751c759175a59638a
JulianCedric <julian.pormentilla@gmail.com>
2020-04-27 18:49:31 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,30 @@
 class Student
+    attr_reader :first_name
+
+    @@all = [] 
+
+    def initialize(first_name)
+        @first_name = first_name
+        Student.all << self 
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def add_boating_test(name, status, instructor)
+        BoatingTest.new(name, status, self, instructor)
+    end
+
+    def self.find_student(first_name)
+        x = Student.all.find do |student|
+            student.first_name == first_name 
+        end 
+        x.map do |elem|
+            puts elem 
+        end 
+    end
+
+    # last method tests logic.
 
 end
