6f0748bef384b063fd190dca4ef9f735952086c7
github username <github email address>
2020-04-27 19:25:03 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,26 @@
 class Student
 
+@@all = []
+attr_reader :first_name
+def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+ end
+ def self.all
+    @@all
+ end
+
+
+def add_boating_test(test5)
+
+test5 = BoatingTest.new(leornad,89, "pending", puff)   
+
+end
+def self.find_student
+    Student.all.select do |v|
+        v.first_name = student
+
+end 
+end
+
 end
