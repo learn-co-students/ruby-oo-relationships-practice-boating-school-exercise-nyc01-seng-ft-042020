ead276a838b327f1d819fbe938f07660484ef0bc
blobbyblobfish <aywang6@gmail.com>
2020-04-27 17:44:11 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Instructor
+
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        Instructor.all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def pass_student(student, name)
+        my_test = BoatingTest.all.find do |test| 
+            (test.student == student && test.name == name)
+        end
+        my_test.status = "passed"
+    end
+
+    def fail_student(student, name)
+        my_test = BoatingTest.all.find {|test| test.student == student && test.name == name}
+        my_test.status = "failed"
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,23 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+ 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+no_crashing = spongebob.add_boating_test(student: spongebob, name: "Don't Crash 101", status: "pending", teacher: puff)
+power_steering_failure = patrick.add_boating_test(student: patrick, name: "Power Steering 202", status: "failed", teacher: puff)
+power_steering_pass = patrick.add_boating_test(student: patrick, name: "Power Steering 201", status: "passed", teacher: krabs)
+
+no_crashing #=> status = "pending"
+puff.fail_student(spongebob, "Don't Crash 101")
+binding.pry
+no_crashing #=> status = "failed"
+puff.pass_student(spongebob, "Don't Crash 101")
+no_crashing #=> status = "passed"
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
