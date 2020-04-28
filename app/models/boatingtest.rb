d2b533143321696616abaa848de0479fb26404ca
aromsa <annacbateman@gmail.com>
2020-04-27 21:25:11 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,33 @@
 class BoatingTest
+    attr_accessor :student, :test_name, :test_status, :instructor
+    @@all = []
+    def initialize (student, test_name, test_status, instructor)
+        @student = student
+        @test_name = test_name
+        @test_status = test_status
+        @instructor = instructor
+        BoatingTest.all << self
+    end
+    def self.all
+        @@all
+    end
 
+
+    def self.find_by_student_and_test_name(student, test_name)
+      boating_test = self.all.find do |boating_test|
+        boating_test.student == student && boating_test.test_name == test_name  
+      end
+    end
+
+    def self.find_all_by_student(student)
+      boating_test = self.all.select do |boating_test|
+        boating_test.student == student 
+      end
+    end
+  
 end
+
+
+# DONE: BoatingTest` class:
+# DONE:  * should initialize with Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+# DONE:  * `BoatingTest.all` returns an array of all boating tests
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,44 @@
 class Instructor
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        Instructor.all << self
+    end
+    def self.all
+        @@all
+    end
 
+    def pass_student(student, test_name)    
+        boating_test = BoatingTest.find_by_student_and_test_name
+        if !boating_test
+            boating_test = BoatingTest.new(student, test_name)
+        end
+        boating_test.test_status = "passed"
+        boating_test
+    end
+
+    def fail_student(student, test_name)
+        # we need the boating_test instance
+        # were looking for the boating test whose student matches
+        # the student that was passed in as an argument.
+
+        boating_test = BoatingTest.find_by_student_and_test_name(student, test_name)
+
+        if !boating_test
+            boating_test = BoatingTest.new(student, test_name)
+        end
+
+        #change the test_status of boating test inst. to = "passed"
+        boating_test.test_status = "failed"
+
+        # this methods needs to return
+        # the boating_test instance
+        boating_test
+    end
 end
+
+# DONE * initialize with a name
+# DONE * `Instructor.all` should return all instructors
+# * `Instructor#pass_student` should take in a student instance and test name. If there is a `BoatingTest` whose name and student match the values passed in, this method should update the status of that BoatingTest to 'passed'. If there is no matching test, this method should create a test with the student, that boat test name, and the status 'passed'. Either way, it should return the `BoatingTest` instance.
+# * `Instructor#fail_student` should take in a student instance and test name. Like `#pass_student`, it should try to find a matching `BoatingTest` and update its status to 'failed'. If it cannot find an existing `BoatingTest`, it should create one with the name, the matching student, and the status 'failed'.
